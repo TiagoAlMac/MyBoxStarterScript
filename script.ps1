407d7262048de4388011cf581206806a516e3fbb
Tiago Machado <tiagoalmachado@outlook.com>
2018-06-11 22:03:40 -0300

    adicionando script

---
+++ script.ps1
@@ -0,0 +1,33 @@
+Disable-UAC
+
+#--- Windows Features ---
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+#--- File Explorer Settings ---
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+#--- Windows Subsystems/Features ---
+choco install -y Microsoft-Hyper-V-All -source windowsFeatures
+choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+
+#--- Define Packages to Install ---
+$Packages = 'git',`
+            'poshgit',`
+            'vscode',`
+            'nodejs',`
+            'FiraCode',`
+            'visualstudio2017community',`
+            'hyper',`
+            'firefox',`
+            'sass'
+
+#--- Install Packages ---
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
